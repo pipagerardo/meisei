0ded5b4b71e5a3d683b369e586087710b0046dc2
PipaGerardo <pipagerardo@gmail.com>
2024-05-08 21:17:06 +0200

    Add files via upload

--- projects/5 - limpia-clean.bat
+++ projects/5 - limpia-clean.bat
@@ -1,7 +1,7 @@
 DEL /Q *.depend 
 DEL /Q *.layout
 DEL /S /Q ..\obj\*.*
-RMDIR /S /Q ..\obj\esp_ucrt64bits
-RMDIR /S /Q ..\obj\eng_ucrt64bits
+RMDIR /S /Q ..\obj\esp_64bits
+RMDIR /S /Q ..\obj\eng_64bits
 RMDIR /S /Q ..\obj\esp_32bits
 RMDIR /S /Q ..\obj\eng_32bits
