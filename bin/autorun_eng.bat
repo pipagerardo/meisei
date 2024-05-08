38aa82679f49dc0eb80720e1d213490b4bb3c5d9
PipaGerardo <pipagerardo@gmail.com>
2024-05-08 21:03:31 +0200

    Add files via upload

---
+++ bin/autorun_eng.bat
@@ -0,0 +1,9 @@
+REM If there is an arcivo called "meisei_autorun.rom" or "meisei_autorun.zip"
+REM starts automatically when you start the meisei emulator.
+REM can only be a ROM or a DSK, if it's a DSK compress it to ZIP.
+
+Meisei_eng.exe games\rom\MJTT.zip
+
+pause
+
+
---
+++ bin/autorun_esp.bat
@@ -0,0 +1,9 @@
+REM Si hay un arcivo llamado "meisei_autorun.rom" o "meisei_autorun.zip"
+REM se inicia automáticamene al arrancar el emulador meisei.
+REM Sólo puede ser un ROM o un DSK, si es un DSK comprímelo a ZIP.
+
+Meisei_esp.exe games\rom\MJTT.zip
+
+pause
+
+
