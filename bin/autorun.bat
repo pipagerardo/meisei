f5374bc4c08aa9b7b7ca700977190923eb917829
PipaGerardo <pipagerardo@gmail.com>
2024-05-08 21:02:53 +0200

    Delete bin/autorun.bat

--- bin/autorun.bat
+++
@@ -1,6 +0,0 @@
-REM Si hay un arcivo llamado "meisei_autorun.rom" o "meisei_autorun.zip"
-REM se inicia automáticamene al arrancar el emulador meisei.
-REM Sólo puede ser un ROM o un DSK, si es un DSK comprímelo a ZIP.
-
-Meisei_esp_64bits.exe games\rom\MJTT.zip
-
