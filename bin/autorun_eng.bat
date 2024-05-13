45b398604c626da105421a7a52437aa94cacccf8
pipagerardo <pipagerardo@gmail.com>
2024-05-13 19:45:25 +0200

    13 mayo 2024

--- bin/autorun_eng.bat
+++ bin/autorun_eng.bat
@@ -2,7 +2,7 @@ REM If there is an arcivo called "meisei_autorun.rom" or "meisei_autorun.zip"
 REM starts automatically when you start the meisei emulator.
 REM can only be a ROM or a DSK, if it's a DSK compress it to ZIP.
 
-Meisei_eng.exe games\rom\MJTT.zip
+Meisei_eng.exe games\rom\Westen House EX - Brain Games (2023).zip
 
 pause
 
--- bin/autorun_esp.bat
+++ bin/autorun_esp.bat
@@ -2,7 +2,7 @@ REM Si hay un arcivo llamado "meisei_autorun.rom" o "meisei_autorun.zip"
 REM se inicia automáticamene al arrancar el emulador meisei.
 REM Sólo puede ser un ROM o un DSK, si es un DSK comprímelo a ZIP.
 
-Meisei_esp.exe games\rom\MJTT.zip
+Meisei_esp.exe games\rom\Westen House EX - Brain Games (2023).zip
 
 pause
 
